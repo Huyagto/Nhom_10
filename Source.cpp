1ee06cb2d61f5ce125af883469466e0a5cbbc8e2
Gia Huy <giahuynguyen9700@gmail.com>
2025-11-04 10:41:03 +0700

    ham tong

--- Source.cpp
+++ Source.cpp
@@ -2,13 +2,19 @@
 
 using namespace std;
 int tong(int a, int b);
+	return a + b;
 int hieu(int a, int b);
 int tich(int a, int b);
 float thuong(int a, int b);
 int main()
 {
+	int x,y;
 	cout << "DAY LA CHUONG TRINH DE THUC HANH GIT";
-	cout<<"Tong =";
+	cout << "Nhap a: ";
+	cin >> x;
+	cout << "Nhap b: ";
+	cin >> y;
+	cout<<"Tong = " << tong(x,y) << endl;
 	system("pause");
 	return 0;
 }
