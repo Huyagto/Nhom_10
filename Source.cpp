22c6b9a8df89e0cf96a7637045fec6dd8f60248e
Gia Huy <giahuynguyen9700@gmail.com>
2025-11-04 11:22:21 +0700

    tongson

--- Source.cpp
+++ Source.cpp
@@ -2,21 +2,60 @@
 
 using namespace std;
 int tong(int a, int b);
-	return a + b;
 int hieu(int a, int b);
 int tich(int a, int b);
-	return a * b;
 float thuong(int a, int b);
 int main()
 {
 	int x,y;
 	cout << "DAY LA CHUONG TRINH DE THUC HANH GIT";
+	cout<<"Tong 2 so: ";
 	cout << "Nhap a: ";
 	cin >> x;
 	cout << "Nhap b: ";
 	cin >> y;
 	cout<<"Tong = " << tong(x,y) << endl;
+	system("pause");
+	cout<<"Tich 2 so: ";
+	cout << "Nhap a: ";
+	cin >> x;
+	cout << "Nhap b: ";
+	cin >> y;
 	cout<<"Tich = " << tich(x,y) <<endl;
 	system("pause");
+	cout<<"Hieu a - b: ";
+	cout << "Nhap a: ";
+	cin >> x;
+	cout << "Nhap b: ";
+	cin >> y;
+	cout<<"Hieu a-b: "<<hieu(x,y)<<endl;
+	system("pause");
+	cout<<"Thuong a / b: ";
+	cout << "Nhap a: ";
+	cin >> x;
+	cout << "Nhap b: ";
+	cin >> y;
+	cout<<"Hieu a-b: "<<hieu(x,y)<<endl;
 	return 0;
+	
+}
+
+int hieu(int a,int b){
+	return a-b;
+}
+int tong(int a, int b){
+	return a + b;
+}
+int tich(int a, int b){
+	return a * b;
+}
+float thuong(int a, int b){
+	return a / b;
+}
+int tongson(int n){
+	int tong = 0 ;
+	for(int i = 1 ; i<=n ;i++){
+		tong+=i;
+}	
+ 	return tong;
 }
